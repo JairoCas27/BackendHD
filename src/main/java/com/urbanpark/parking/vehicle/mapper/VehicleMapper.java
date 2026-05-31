e50b83807cf1d92473ad7dd269c98e82921d3710
Cristian Candela <cristiancandela561@gmail.com>
2026-05-30 20:32:36 -0500

    feat/ vehiculos map

---
+++ src/main/java/com/urbanpark/parking/vehicle/mapper/VehicleMapper.java
@@ -0,0 +1,25 @@
+package main.java.com.urbanpark.parking.vehicle.mapper;
+ 
+import com.urbanpark.parking.vehicle.domain.model.Vehicle;
+import com.urbanpark.parking.vehicle.dto.response.VehicleResponse;
+import org.springframework.stereotype.Component;
+ 
+@Component
+public class VehicleMapper {
+ 
+    public VehicleResponse toResponse(Vehicle vehicle) {
+        return new VehicleResponse(
+            vehicle.getId(),
+            vehicle.getTenantId(),
+            vehicle.getPlate(),
+            vehicle.getBrand(),
+            vehicle.getModel(),
+            vehicle.getColor(),
+            vehicle.getType(),
+            vehicle.getOwner().getId(),
+            vehicle.getOwner().getName(),
+            vehicle.getIsActive(),
+            vehicle.getCreatedAt()
+        );
+    }
+}
