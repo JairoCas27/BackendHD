5c3736946c562f979b87e560e7b8c1db84e8f6d8
BrunoTC725 <brunotc7365@gmail.com>
2026-05-30 23:49:05 -0500

    actualización al tiempo de acceso condominio

--- src/main/java/com/urbanpark/parking/rules/RestriccionesCondominio.java
+++ src/main/java/com/urbanpark/parking/rules/RestriccionesCondominio.java
@@ -1,38 +1,78 @@
 package main.java.com.urbanpark.parking.rules;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
-import java.math.BigDecimal;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalTime;
 
 @Entity
 @Table(name = "restricciones_condominio")
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class RestriccionesCondominio {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
-    @Column(name = "condominio_id")
+
+    @Column(name = "condominio_id", nullable = false, unique = true)
     private Long condominioId;
-    
-    @Column(name = "max_parqueos_por_apartamento")
-    private Integer maxParqueosPorApartamento;
-    
-    @Column(name = "max_visitantes_simultaneos")
-    private Integer maxVisitantesSimultaneos;
-    
-    @Column(name = "tiempo_maximo_visita_horas")
-    private Integer tiempoMaximoVisitaHoras;
-    
-    @Column(name = "costo_parqueo_visitante")
-    private BigDecimal costoParqueoVisitante;
-    
-    @Column(name = "requiere_reserva_previa")
+
+    @Column(name = "max_vehiculos_por_usuario", nullable = false)
+    private Integer maxVehiculosPorUsuario;
+
+    @Column(name = "max_visitas_por_dia", nullable = false)
+    private Integer maxVisitasPorDia;
+
+    @Column(name = "hora_apertura", nullable = false)
+    private LocalTime horaApertura;
+
+    @Column(name = "hora_cierre", nullable = false)
+    private LocalTime horaCierre;
+
+    @Column(name = "permite_visitas_fin_de_semana", nullable = false)
+    private Boolean permiteVisitasFinDeSemana;
+
+    @Column(name = "tiempo_maximo_estacionamiento_horas", nullable = false)
+    private Integer tiempoMaximoEstacionamientoHoras;
+
+    @Column(name = "requiere_reserva_previa", nullable = false)
     private Boolean requiereReservaPrevia;
-    
-    @Column(name = "permite_acceso_24h")
-    private Boolean permiteAcceso24h;
-    
-    private Boolean activo;
+
+    @Column(name = "tarifa_por_hora", nullable = false)
+    private Double tarifaPorHora;
+
+    @Column(name = "tarifa_visitante_por_hora", nullable = false)
+    private Double tarifaVisitantePorHora;
+
+    @Column(name = "activo", nullable = false)
+    private Boolean activo = true;
+
+    // Métodos de negocio
+    public boolean estaDentroHorarioPermitido(LocalTime hora) {
+        return !hora.isBefore(horaApertura) && !hora.isAfter(horaCierre);
+    }
+
+    public boolean puedeRegistrarVehiculo(int vehiculosActuales) {
+        return vehiculosActuales < maxVehiculosPorUsuario;
+    }
+
+    public boolean permiteVisita(int visitasDelDia, boolean esFinDeSemana) {
+        if (esFinDeSemana && !permiteVisitasFinDeSemana) {
+            return false;
+        }
+        return visitasDelDia < maxVisitasPorDia;
+    }
+
+    public double calcularTarifa(boolean esVisitante, int horas) {
+        if (esVisitante) {
+            return tarifaVisitantePorHora * horas;
+        }
+        return tarifaPorHora * horas;
+    }
 }
