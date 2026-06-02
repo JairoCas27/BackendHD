125e2c8c6abee80d74c67a273fc6b682ab01f2e8
Jeancarlo Mejia <u23211982@utp.edu.pe>
2026-06-01 22:55:54 -0500

    Entidad Condominio y repositorio agregado

--- src/main/java/com/urbanpark/parking/domain/tenant/CondominioRepository.java
+++ src/main/java/com/urbanpark/parking/domain/tenant/CondominioRepository.java
@@ -1,4 +1,14 @@
 package com.urbanpark.parking.domain.tenant;
 
-public class CondominioRepository {
+import com.urbanpark.parking.shared.enums.EstadoCondominio;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface CondominioRepository extends JpaRepository<Condominio, UUID> {
+    Optional<Condominio> findByTitularEmail(String email);
+    List<Condominio> findAllByEstado(EstadoCondominio estado);
+    boolean existsByTitularEmail(String email);
 }
