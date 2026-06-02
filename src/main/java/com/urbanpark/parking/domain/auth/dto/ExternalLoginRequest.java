102d6d5521b525d506eea59d6bf803d09976cf45
Jeancarlo Mejia <u23211982@utp.edu.pe>
2026-06-01 23:10:48 -0500

    Request del login usando la api del gestor de condominios

--- src/main/java/com/urbanpark/parking/domain/auth/dto/ExternalLoginRequest.java
+++ src/main/java/com/urbanpark/parking/domain/auth/dto/ExternalLoginRequest.java
@@ -1,4 +1,21 @@
 package com.urbanpark.parking.domain.auth.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import java.util.UUID;
+
+@Data
 public class ExternalLoginRequest {
+
+    @Email(message = "Email inválido")
+    @NotBlank(message = "El email es obligatorio")
+    private String email;
+
+    @NotBlank(message = "El password es obligatorio")
+    private String password;
+
+    @NotNull(message = "El tenantId es obligatorio")
+    private UUID tenantId;
 }
