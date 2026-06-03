4cf6453739f32a5929532954f0ecca4a014ff24b
NickAyala12 <165934404+NickAyala12@users.noreply.github.com>
2026-06-03 16:41:46 -0500

    feat(contactanos): implementar busqueda por codigo de seguimiento

--- src/main/java/com/urbanpark/parking/domain/notifications/contactanos/ContactoMensajeRepository.java
+++ src/main/java/com/urbanpark/parking/domain/notifications/contactanos/ContactoMensajeRepository.java
@@ -1,8 +1,12 @@
 package com.urbanpark.parking.domain.notifications.contactanos;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ContactoMensajeRepository extends JpaRepository<ContactoMensaje, Long> {
+
+Optional<ContactoMensaje> findByCodigoSeguimiento(String codigoSeguimiento);
 }
