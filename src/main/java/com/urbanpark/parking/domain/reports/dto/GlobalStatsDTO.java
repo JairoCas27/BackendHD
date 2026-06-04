b61bd75f8071fda81aee51309c313a1224485c1d
JairoCas27 <u23200267@utp.edu.pe>
2026-06-03 20:22:03 -0500

    DTO envoltorio de estadísticas globales del sistema completo

--- src/main/java/com/urbanpark/parking/domain/reports/dto/GlobalStatsDTO.java
+++ src/main/java/com/urbanpark/parking/domain/reports/dto/GlobalStatsDTO.java
@@ -1,5 +1,18 @@
 package com.urbanpark.parking.domain.reports.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+@Getter @Setter @Builder
+@NoArgsConstructor @AllArgsConstructor
 public class GlobalStatsDTO {
-    
+
+    @JsonProperty("usuarios")
+    private UsuarioStatsDTO usuarioStats;
+
+    @JsonProperty("timestamp")
+    private String timestamp;
+
+    @JsonProperty("periodo")
+    private String periodo;
 }
