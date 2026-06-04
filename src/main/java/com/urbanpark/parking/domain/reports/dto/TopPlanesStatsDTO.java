22f8d5748febaab4d187a4859dec6fa6640e826b
JairoCas27 <u23200267@utp.edu.pe>
2026-06-03 20:25:37 -0500

    DTO envoltorio top 3 planes más adquiridos

--- src/main/java/com/urbanpark/parking/domain/reports/dto/TopPlanesStatsDTO.java
+++ src/main/java/com/urbanpark/parking/domain/reports/dto/TopPlanesStatsDTO.java
@@ -1,5 +1,20 @@
 package com.urbanpark.parking.domain.reports.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+import java.util.List;
+
+@Getter @Setter @Builder
+@NoArgsConstructor @AllArgsConstructor
 public class TopPlanesStatsDTO {
-    
+
+    @JsonProperty("topPlanes")
+    private List<TopPlanDTO> topPlanes;
+
+    @JsonProperty("totalPlanesActivos")
+    private Long totalPlanesActivos;
+
+    @JsonProperty("timestamp")
+    private String timestamp;
 }
